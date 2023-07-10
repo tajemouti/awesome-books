3bdbaf04cceed1b6434361705302604723d9d16b
Mozamel Jawad <mozameljawad@gmail.com>
2023-07-10 21:17:50 +0430

    Add the neccessary variables to index.js file

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
+const title = document.getElementById('book-title');
+const author = document.getElementById('book-author');
+const button = document.getElementById('add-btn');
+const newBooks = document.getElementById('new-books');
 
