47bdfec9ec180a47e940d50e8ac075223fb7830e
tajemouti <130588108+tajemouti@users.noreply.github.com>
2023-07-11 02:37:33 +0100

    Create an array of objects bookList

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const author = document.getElementById('book-author');
 const button = document.getElementById('add-btn');
 const newBooks = document.getElementById('new-books');
 
+if (localStorage.getItem('booksDetails') === null) {
+  localStorage.setItem('booksDetails', JSON.stringify([]));
+}
+
+const bookList = JSON.parse(localStorage.getItem('booksDetails'));
+
+function saveStorage() {
+  localStorage.setItem('booksDetails', JSON.stringify(bookList));
+}
