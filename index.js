f5697ec7262452dbeb1aa84c1e4759705732933b
tajemouti <130588108+tajemouti@users.noreply.github.com>
2023-07-12 03:46:10 +0100

    Add a class to the js file

--- index.js
+++ index.js
@@ -1,51 +1,15 @@
-const title = document.getElementById('book-title');
-const author = document.getElementById('book-author');
+const bookTitle = document.getElementById('book-title');
+const bookAuthor = document.getElementById('book-author');
 const button = document.getElementById('add-btn');
 const newBooks = document.getElementById('new-books');
 
-if (localStorage.getItem('booksDetails') === null) {
-  localStorage.setItem('booksDetails', JSON.stringify([]));
+class Book {
+  bookList;
+
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+    this.getStorage();
+  }
 }
-
-const bookList = JSON.parse(localStorage.getItem('booksDetails'));
-
-function saveStorage() {
-  localStorage.setItem('booksDetails', JSON.stringify(bookList));
-}
-
-function displayBooks() {
-  newBooks.innerHTML = '';
-  bookList.forEach((book, index) => {
-    const shelf = `
-            <div>
-              <p>${book.titleVal}</p>
-              <p>${book.authorVal}</p>
-              <button id=${index} class="btn" type="button">Remove</button>
-            </div>
-      `;
-    newBooks.insertAdjacentHTML('beforeend', shelf);
-    const remBtn = document.getElementById(`${index}`);
-
-    function removeBook() {
-      bookList.splice(this, 1);
-      saveStorage();
-      displayBooks();
-    }
-    remBtn.addEventListener('click', removeBook.bind(index));
-  });
-}
-
-button.addEventListener('click', (event) => {
-  event.preventDefault();
-
-  const titleVal = title.value;
-  const authorVal = author.value;
-
-  const book = { titleVal, authorVal };
-  bookList.push(book);
-
-  saveStorage(bookList);
-  displayBooks();
-});
-
-displayBooks(bookList);
+  
