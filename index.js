9ecabd60567a2672cd7f0f5a1d6a9987f6b5bcb8
tajemouti <130588108+tajemouti@users.noreply.github.com>
2023-07-12 03:50:36 +0100

    Create Add and Remove methods

--- index.js
+++ index.js
@@ -11,5 +11,47 @@ class Book {
     this.author = author;
     this.getStorage();
   }
+
+  getStorage() {
+    this.bookList = JSON.parse(localStorage.getItem('booksDetails')) || [];
+  }
+
+  loadIntoStorage(node) {
+    if (node) {
+      localStorage.setItem('booksDetails', JSON.stringify(node));
+      return;
+    }
+    this.getStorage();
+    const book = { title: this.title, author: this.author };
+    this.bookList.push(book);
+    localStorage.setItem('booksDetails', JSON.stringify(this.bookList));
+  }
+
+  displayBooks() {
+    newBooks.innerHTML = '';
+    if (this.bookList === null) return;
+    this.bookList.forEach((book, index) => {
+      const shelf = `
+      <div class="book">
+        <p class="book-info">"${book.title}" by ${book.author}</p>
+        <button id=${index} class="remove-btn">Remove</button>
+      </div>
+      `;
+
+      newBooks.insertAdjacentHTML('beforeend', shelf);
+    });
+  }
+
+  saveStorage() {
+    this.getStorage();
+    this.displayBooks();
+  }
+
+  removeBook(index) {
+    this.getStorage();
+    const filter = this.bookList.filter((book) => book !== this.bookList.at(index));
+    this.loadIntoStorage(filter);
+    this.saveStorage();
+  }
 }
   
